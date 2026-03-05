a3c85ed8ae38caa4d77512293b4460175f497f6c
vitoroliveira1708 <vitor.ferreira.oliveira17@escola.pr.gov.br>
2026-03-05 11:26:02 -0300

    Create  banco-pessoal.sql

---
+++ banco-pessoal.sql
@@ -0,0 +1,20 @@
+6. Escreva a sua versão para o código do exercício anterior. Copie daqui e cole num bloco de notas para digitar as modificações. A reescrita do código deve manter comandos e tipos de dados, e apresentar:
+- novo nome de tabela;
+- novos nomes para as 4 colunas;
+- ajuste da última linha para exibir a tabela nova. 
+
+CREATE TABLE Produtos (
+    Codigo_Produto INT PRIMARY KEY,
+    Nome_Produto VARCHAR(100) NOT NULL,
+    Preco_Unitario_Kg DECIMAL(10, 2) NOT NULL,
+    Comentarios TEXT
+);
+SELECT * FROM Produtos; 
+
+DICA: o teste do código deve ter a mesma saída da imagem.
+
+Depois de revisar se tudo está compatível, teste o seu código no editor SQLite Online (limpe a tela para usar) e copie o link de compartilhamento da execução correta do código.
+*
+12 pontos
+Imagem sem legenda
+nao consegui fazer
