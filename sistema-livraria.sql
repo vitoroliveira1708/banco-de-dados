0dcdf1d8d3a13143cf72ce7c48f6f27ba9004889
vitoroliveira1708 <vitor.ferreira.oliveira17@escola.pr.gov.br>
2026-06-26 11:28:46 -0300

    Create  sistema-livraria.sql

---
+++ sistema-livraria.sql
@@ -0,0 +1,15 @@
+SELECT * FROM demo;
+-- Criação da tabela Clientes
+CREATE TABLE Clientes (
+    ID INTEGER PRIMARY KEY AUTOINCREMENT,
+    nomeCliente TEXT NOT NULL,
+    emailCliente TEXT UNIQUE
+);
+
+-- Criação da tabela Compras
+CREATE TABLE Compras (
+    CompraID INTEGER PRIMARY KEY AUTOINCREMENT,
+    ClienteID INTEGER NOT NULL,
+    NomeLivro TEXT NOT NULL,
+    FOREIGN KEY (ClienteID) REFERENCES Clientes(ID)
+);
